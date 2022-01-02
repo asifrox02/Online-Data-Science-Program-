a4db9655a7e848dbf42830a35f57367c9e7fd0c7
Muhammad Asif <asifrox02@gmail.com>
2022-01-02 16:19:06 +0500

    Assignment Complete

---
+++ Assignment # 1/Assignment No 01.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Twinkle, twinkle, little star, \n\t How I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\t How I wonder what you are!")
+
+
+# In[2]:
+
+
+print("Python 3.9.7")
+
+
+# In[4]:
+
+
+import datetime
+now = datetime.datetime.now()
+print ("Current date and time : ")
+print (now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+# In[5]:
+
+
+from math import pi
+radius = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(radius) + " is: " + str(pi * radius**2))
+
+
+# In[6]:
+
+
+a = (" Muhammad");
+b = ("Asif");
+print(b + a);
+
+
+# In[7]:
+
+
+a = (10);
+b = (8);
+c = (b + a);
+print(c);
+
+
+# In[ ]:
+
+
+
+
